83ace3830aaf4033ef2ceb1bb3082ce1d856bf15
David Reguera <birrick@gmail.com>
2021-01-12 17:29:33 +0100

    Clean booking data

--- clean_csv.py
+++ clean_csv.py
@@ -22,9 +22,132 @@ def main():
         return clean_sales(args.raw_csv)
 
 
-def clean_bookings(source):
-    """Clean the bookings data."""
-    print("cleaning bookings source {}.".format(source))
+def clean_bookings(source: str) -> pd.DataFrame:
+    """Clean the bookings data.
+
+    Arguments:
+    source, a path like string that points to a csv file
+
+    Returns:
+    A pandas DataFrame object with the data ready to be uploaded.
+    """
+
+
+    col_map = {
+        'ID de reserva': 'id',
+        '¿Cancelado?': 'cancelled',
+        'Creado en hora': 'create_time',
+        'Creado en fecha': 'create_date',
+        'Reservado por': 'booked_by',
+        'ID de artículo': 'article_id',
+        'Artículo': 'article',
+        'Hora de inicio': 'start_hour',
+        'Fecha de inicio': 'start_date',
+        'Día de disponibilidad': 'start_day',
+        'Encabezado': 'public_header',
+        'Encabezado privado': 'private_header',
+        'Contacto': 'contact',
+        'Teléfono': 'phone',
+        'Idioma de contacto': 'language',
+        '¿Suscrito a mensajes de texto?': 'opt_in_txt',
+        'E-mail': 'email',
+        '¿Está suscrito a e-mail?': 'opt_in_email',
+        'Notas de reserva': 'notes',
+        'N.º de pasajeros': 'pax',
+        'Referencia de reserva online': 'online_ref',
+        'Hoja de total': 'price_sheet',
+        'Subtotal': 'subtotal',
+        'Impuesto (21 %)': 'tax_21',
+        'Impuesto total': 'tax_total',
+        'Total': 'total',
+        'Subtotal pagado': 'subtotal_paid',
+        'Impuesto (21 %) pagado': 'tax_21_paid',
+        'Impuestos totales pagados': 'tax_total_paid',
+        'Total pagado': 'total_paid',
+        'Subtotal pagado a Afiliado': 'subtotal_paid_affiliate',
+        'Impuestos pagados a Afiliado': 'tax_paid_affiliate',
+        'Total pagado a Afiliado': 'total_paid_affiliate',
+        'Ingresos netos Cobrados': 'net_profit',
+        'Gastos de gestión': 'tpv_charge',
+        'Total pagado tras los gastos de gestión': 'total_paid_after_tpv',
+        'Gastos de gestión Cobrado al Afiliado': 'tpv_charged_to_affiliate',
+        'Total pagado al Afiliado tras los gastos de gestión':
+            'total_paid_affiliate_after_tpv',
+        'Cantidad debida': 'debt_amount',
+        'Estado de pago': 'payment_status',
+        'Afiliado': 'affiliate',
+        'Cupón': 'voucher',
+        'Descripción de hoja de factura': 'sheet_description',
+        'Hoja de factura': 'invoice_sheet',
+        'Factura Subtotal': 'invoice_subtotal',
+        'Impuesto de factura': 'invoice_tax',
+        'Total de factura': 'invoice_total',
+        '¿Facturado?': 'invoiced',
+        'A pagar al afiliado': 'affiliate_to_be_paid',
+        'Pagado a Afiliado': 'affiliate_paid',
+        'Por cobrar al afiliado': 'affiliate_pending',
+        'Recibido de Afiliado': 'affiliate_received'
+    }
+
+    # If any of the columns in the col_map is missing, it will raise value
+    # error, whereas if there are more on the csv file they won't be used. This
+    # way we ensure that only the columns in the map are used, no more no less
+    bookings_raw = pd.read_csv(
+        source, header=1, engine='python', usecols=col_map.keys())
+
+    bookings = bookings_raw.rename(columns=col_map)
+
+    if bookings.pax.dtype != 'int64':
+        raise ValueError("Pax should be an int")
+
+    # Last row is likely to have totals and therefore won't have booking id so
+    # get rid of it
+    last_row = bookings.iloc[-1:].index
+    f = bookings.index.isin(last_row) & bookings.id.isna()
+    if f.sum():
+        bookings.drop(index=bookings[f].index, inplace=True)
+
+    # Ensure there are no more nan ids across the dataframe
+    if not bookings[bookings.id.isna()].empty:
+        raise ValueError("There are rows with no id.")
+
+    # Transform float columns
+    float_cols = [
+        'subtotal',
+        'tax_21',
+        'tax_total',
+        'total',
+        'subtotal_paid',
+        'tax_21_paid',
+        'tax_total_paid',
+        'total_paid',
+        'subtotal_paid_affiliate',
+        'tax_paid_affiliate',
+        'total_paid_affiliate',
+        'net_profit',
+        'tpv_charge',
+        'total_paid_after_tpv',
+        'tpv_charged_to_affiliate',
+        'total_paid_affiliate_after_tpv',
+        'debt_amount',
+        'invoice_subtotal',
+        'invoice_tax',
+        'invoice_total',
+        'affiliate_to_be_paid',
+        'affiliate_paid',
+        'affiliate_pending',
+        'affiliate_received', ]
+
+    for col in float_cols:
+        bookings.loc[:, col] = bookings[col].str.replace('.', '')
+        bookings.loc[:, col] = bookings[col].str.replace(',', '.')
+        bookings.loc[:, col] = bookings[col].str.replace('€', '').str.strip()
+        bookings.loc[:, col] = bookings[col].astype(float)
+
+    # Convert ids to ints
+    bookings.id = bookings.id.str.replace('#', '').astype(int)
+
+    return bookings
 
 
 def clean_sales(source):
---
+++ tests.py
@@ -0,0 +1,94 @@
+import unittest
+import os
+import csv
+import pandas as pd
+
+from clean_csv import clean_bookings, clean_sales, create_upload
+
+
+class TestCleanBookings(unittest.TestCase):
+    """Test the cleaning of the files."""
+
+    def test_less_columns_than_expected_raises_error(self):
+        source = "./sample_test_files/less_columns_bookings.csv"
+        with open(source, "r") as f:
+            msg = "Usecols do not match columns"
+            with self.assertRaisesRegex(ValueError, msg):
+                clean_bookings(f)
+
+    def test_not_int_pax_raises_error(self):
+        source = "./sample_test_files/pax_not_int.csv"
+        with self.assertRaisesRegex(ValueError, "Pax should be an int"):
+            clean_bookings(source)
+
+    def test_nan_ids_are_only_allowed_in_the_last_row(self):
+        source = "./sample_test_files/nan_ids_between_bookings.csv"
+        msg = "There are rows with no id."
+        with self.assertRaisesRegex(ValueError, msg):
+            clean_bookings(source)
+
+    def test_valid_csv_returns_a_pandas_dataframe(self):
+        source = "./sample_test_files/valid_bookings.csv"
+        df = clean_bookings(source)
+        self.assertIsInstance(df, pd.DataFrame)
+
+
+class TestCreateUpload(unittest.TestCase):
+
+    def test_create_upload_filename_should_be_bookings_or_sales(self):
+        msg = "Filename should be either bookings or sales."
+        with self.assertRaisesRegex(ValueError, msg):
+            create_upload("foo", "bar")
+
+    def test_create_upload_creates_a_valid_csv_file_for_bookings(self):
+        source = "./sample_test_files/valid_bookings.csv"
+        df = clean_bookings(source)
+        create_upload(df, "bookings")
+        self.assertTrue(os.path.exists("bookings.csv"))
+        with open("bookings.csv", "r") as f:
+            reader = csv.reader(f, delimiter=",")
+            _, values = [row for row in reader]
+
+            expected = [
+                '36816723', 'No', '16:43', '2019-05-27', 'Aila a', '#153',
+                'bao', '10:30', '2019-05-28', 'Mtes', 'sh', '', 'Hrtz',
+                '+49 1648', 'Esl', 'N', 'om', '', '', '2', '', 'Direct',
+                '52.9', '0.0', '11.1', '64.0', '52.9', '0.0', '11.1', '64.0',
+                '52.9', '11.1', '64.0', '44.8', '0.0', '64.0', '0.0', '64.0',
+                '0.0', 'paid', '', '7', '70% rate, taxed', 'Billing - 30%',
+                '37.04', '7.76', '44.8', 'No', '0.0', '0.0', '44.8', '0.0']
+
+            for v, e in zip(values, expected):
+                self.assertEqual(v, e)
+
+    def test_create_upload_creates_a_sql_query_for_bookings(self):
+        source = "./sample_test_files/valid_bookings.csv"
+        df = clean_bookings(source)
+        create_upload(df, "bookings")
+        self.assertTrue(os.path.exists("bookings.sql"))
+        with open("bookings.sql", "r") as f:
+            lines = f.readlines()
+            db_fields = [
+                'id', 'cancelled', 'create_time', 'create_date', 'booked_by',
+                'article_id', 'article', 'start_hour', 'start_date',
+                'start_day', 'public_header', 'private_header', 'contact',
+                'phone', 'language', 'opt_in_txt', 'email', 'opt_in_email',
+                'notes', 'pax', 'online_ref', 'price_sheet', 'subtotal',
+                'tax_21', 'tax_total', 'total', 'subtotal_paid', 'tax_21_paid',
+                'tax_total_paid', 'total_paid', 'subtotal_paid_affiliate',
+                'tax_paid_affiliate', 'total_paid_affiliate', 'net_profit',
+                'tpv_charge', 'total_paid_after_tpv',
+                'tpv_charged_to_affiliate', 'total_paid_affiliate_after_tpv',
+                'debt_amount', 'payment_status', 'affiliate', 'voucher',
+                'sheet_description', 'invoice_sheet', 'invoice_subtotal',
+                'invoice_tax', 'invoice_total', 'invoiced',
+                'affiliate_to_be_paid', 'affiliate_paid', 'affiliate_pending',
+                'affiliate_received', ]
+            self.assertEqual(
+                lines[0], "COPY bookings({})\n".format(','.join(db_fields)))
+            self.assertEqual(lines[1], "FROM '/home/redash/bookings.csv'\n")
+            self.assertEqual(lines[2], "DELIMITER ','\n")
+            self.assertEqual(lines[3], "CSV HEADER;")
+
+if __name__ == "__main__":
+    unittest.main()
